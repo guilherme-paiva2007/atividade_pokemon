cffbe90490239735ffff49bba6aac72163e2422f
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2025-07-29 10:07:07 -0300

    .

--- index.php
+++ index.php
@@ -4,8 +4,25 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Início - PokeLost</title>
+        <link rel="stylesheet" href="./styles/index.css">
     </head>
     <body>
-        
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="./">Início</a></li>
+                    <li><a href="./cadastrar.php">Cadastrar</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="container-content">
+                    <div class="search-container">
+                        <input type="text" id="input-search-pokemon">
+                    </div>
+                </div>
+            </div>
+        </main>
     </body>
 </html>
